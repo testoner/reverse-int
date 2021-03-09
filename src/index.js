a766cd2e066056f96dabd8d035eca9aa7a106f5a
testoner <ekgeretoker@gmail.com>
2021-03-10 00:57:51 +0300

    refactor: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let k = n;
+    n = n + '';
+    n = n.split('').reverse();
+    let l = n.length;
+    if (k < 0) n.pop();
+    n = n.join('');
+    return +n;
 }
